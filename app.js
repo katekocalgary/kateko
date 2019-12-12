7e99277f7504bd44550e9ca775052ff6713bbb1b
Kate <55151099+katekocalgary@users.noreply.github.com>
2019-12-12 12:44:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(function(req, res, next) {
 });
 
 // Set PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
